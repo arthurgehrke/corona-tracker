ea15cd979a36e25bac8f61a107355049008f0942
arthurgehrke <arthurgehrke07@gmail.com>
2020-04-12 17:35:23 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'React'
+import React from 'react'
 
-class App extends React.component{
+import { Cards, Chart, CountryPicker } from './components'
+
+class App extends React.Component{
   render(){
     return(
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
 }
 
-export default App
+export default App;
